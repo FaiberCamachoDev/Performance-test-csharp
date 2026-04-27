338d9d4bc2c4c291af9de605fe16c4512319ca07
echandia27 <jp.echandia@gmail.com>
2026-04-27 14:40:15 -0500

    initial set up (nugets installation and connection to db on vps)

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Performance_test_csharp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
